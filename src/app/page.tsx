983e086210f6b4c0923e136090d91ddd15434da8
YeonjuSeo <tjduswn1219@gmail.com>
2025-08-21 19:49:30 +0900

    feat: 자산 현황 계산 화면

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,51 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import {
+  Card,
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+} from "@/components/ui/card";
+
+const goal자산 = 100000000;
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [자산, set자산] = useState(1100);
+  const [목표저축액, set목표저축액] = useState(1000000);
+  const [경과달수, set경과달수] = useState(1);
+  const [예상달성기한, set예상달성기한] = useState(12); // 단위는 달
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [예금, set예금] = useState(0);
+  const [주식, set주식] = useState(0);
+  const [적금, set적금] = useState(0);
+  const [기타, set기타] = useState(0);
+
+  const 총자산 = 예금 + 주식 + 적금 + 기타;
+
+  return (
+    <div className="font-sans flex flex-col gap-y-[10px] items-center">
+      <Card className="bg-emerald-500 w-full">
+        <CardHeader>
+          <CardTitle>지금 내 자산</CardTitle>
+          <CardDescription>{총자산.toLocaleString()}원</CardDescription>
+        </CardHeader>
+      </Card>
+      <div>
+        <div>매달 {목표저축액.toLocaleString()}원 씩 저축하면</div>
+        <div>
+          {goal자산.toLocaleString()}원까지 {goal자산 / 목표저축액}개월 만에
+          달성할 수 있습니다.
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {(goal자산 / 예상달성기한) * 경과달수 > 총자산 ? (
+          <div>이대로라면 예상 달성 기한 내에 1억을 모을 수 없어요</div>
+        ) : (
+          <div>이대로라면 예정대로/예정보다 더 빨리 1억을 모을 수 있어요</div>
+        )}
+      </div>
+      <div></div>
     </div>
   );
 }
